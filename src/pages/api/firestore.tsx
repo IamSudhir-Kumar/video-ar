dc3aba828d4d5a9cd055c6e854ce87e48285c48b
SUDHIR KUMAR <sudhirk33835@gmail.com>
2024-07-01 12:05:25 +0530

    chore: Remove unused files and update dependencies

--- src/pages/api/firestore.tsx
+++
@@ -1,6 +0,0 @@
-import {storage , app, database} from "@/firebaseConfig"
-import { NextApiRequest, NextApiResponse } from "next"
-const firestore = (req:NextApiRequest, res:NextApiResponse) => {
-    res.status(200).json({ text: "Hello"})
-};
-export default firestore
